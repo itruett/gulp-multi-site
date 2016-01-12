45231ec49db219efb0953b5f69f246eeb406b353
Isaac Truett <itruett@authxconsulting.com>
2016-01-12 10:11:32 -0500

    Removing some unncessary dependencies and adding M2E config.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,11 +1,8 @@
 // I'm gonna need gulp here.
 var gulp = require('gulp');
 
-// Using this to log to console.
+// Using this to log to console and read CLI araguments.
 var gutil = require('gulp-util');
-
-// Used to parse CLI arguments.
-var minimist = require('minimist');
 
 // Resolves paths on the filesystem.
 var path = require('path')
@@ -13,22 +10,15 @@ var path = require('path')
 // Load project configuration.
 var config = require('./config.json');
 
-// Names of arguments expected by the build.
-var validArgs = {
-	string : 'variant'
-}
-
-// Cut out just the arguments we care about (1st is "node" and 2nd is the gulpfile).
-var options = minimist(process.argv.slice(2), validArgs);
-
 gulp.task('default', function() {
 	// place code for your default task here
 });
 
 gulp.task('build', function() {
-	var variant = options['variant'];
+	var variant = gutil.env.variant;
 	var target = path.resolve(config[variant].target);
 
 	gutil.log('Building variant: ' + variant);
 	gutil.log('Target directory: ' + target);
 });
+
